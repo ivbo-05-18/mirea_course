d92ebf97c4bf7057113ad5d1cdb2c88afe94a838
Kirieraito <10943627+Kirieraito@users.noreply.github.com>
2020-05-03 17:56:09 +0300

    Feature: add Snake game element to personal component

--- src/components/PerederiyVA/Component.js
+++ src/components/PerederiyVA/Component.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Snake from './Snake';
+
+const SNAKE_STYLE = {
+    margin : '30px auto',
+    height : 500,
+    width  : 600
+};
 
 class PerederiyVAElement extends React.Component {
     render(){
-        return (<p>Hello world!</p> )
+        return (
+            <div>
+                <h3>Заимствованный элемент - игра "Змейка"</h3>
+                Управление: W S A D.
+                <div style={SNAKE_STYLE}>
+                    <Snake sound={false} />
+                </div>
+            </div>
+        )
     }
 }
 
