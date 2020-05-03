f93480196ddce1ecf0447f9acf123609f2aa910b
Kirieraito <10943627+Kirieraito@users.noreply.github.com>
2020-05-03 23:27:45 +0300

    Feature: add styles to personal component

--- src/components/PerederiyVA/Component.js
+++ src/components/PerederiyVA/Component.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Snake from './Snake';
 
+const STYLE = {
+    'border-radius' : '2px',
+    background      : '#DFDFDF',
+    margin          : '10px',
+    padding         : '5px 25px'
+}
+
+const TEXT_STYLE = {
+    'font-size' : '16px',
+    color       : 'black',
+    margin      : '10px 0px'
+}
+
 const SNAKE_STYLE = {
-    margin : '30px auto',
+    margin : '10px auto',
     height : 500,
     width  : 600
 };
@@ -10,12 +23,12 @@ const SNAKE_STYLE = {
 class PerederiyVAElement extends React.Component {
     render(){
         return (
-            <div>
-                <h3>Заимствованный элемент - игра "Змейка"</h3>
-                Управление: W S A D.
+            <div style={STYLE}>
+                <h2 style={TEXT_STYLE}>Заимствованный элемент - игра "Змейка"</h2>
+                <p style={TEXT_STYLE}>Управление: W S A D.</p>
                 <div style={SNAKE_STYLE}>
                     <Snake sound={false} />
-                </div>
+                </div>    
             </div>
         )
     }
