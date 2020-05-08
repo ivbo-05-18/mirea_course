a0500a0070fd5bb13387f4bcacfc7b2b9d159a2f
defaulterrr <sp@zero.ru>
2020-05-08 15:36:11 +0300

    [Petrov S.D.] Feature: added conditional component rendering

---
+++ src/components/PetrovSD/PetrovSDElement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PetrovSDElement = () => {
+    return (
+        <div className="petrov_sd_element">
+
+        </div>
+    )
+}
+
+export default PetrovSDElement
