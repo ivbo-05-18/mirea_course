e6d088085fb9712aa86997fc96cefd5d27491781
FairSuspect <fairsuspect@gmail.com>
2020-05-11 12:19:52 +0300

    add external component

---
+++ src/components/VelikanovKY/Trends.js
@@ -0,0 +1,5 @@
+import Trend from 'react-trend';
+import React from 'react';
+const Trends = () => <Trend smooth radius={20} gradient={['#0FF', '#F0F', '#FF0']} data={[0, 10, 5, 22, 3.6, 11]} />;
+
+export default Trends
--- src/components/VelikanovKY/VelikanovKYElement.js
+++ src/components/VelikanovKY/VelikanovKYElement.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Weather from './Weather';
+import Trends from './Trends'
+
 
 const VelikanovKYElement = () => {
 
 
     return(
         <div>
-            <h2> Собственный элемент </h2>               
-                <Weather  />          
+            <h2> Собственный элемент </h2>  
+                <Weather  /> 
+            
             <hr />
-            <h2 >Элемент из-внеп</h2>
+            <h2 >Элемент из-вне</h2>
+                <Trends />
+            <hr />
         </div>
     )
 }
