4161dc07d78b5479a213f0ec7cca43c8b3c2f8c2
Vyacheslav <slava.klevleev@gmail.com>
2020-05-18 18:15:25 +0300

    Add component from GitHub
    Second сomponent taken from repository https://github.com/chuckha/ColorFlood

--- src/components/KlevleevVR/KlevleevVRElement.js
+++ src/components/KlevleevVR/KlevleevVRElement.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GuessTheSong from "./GuessTheSong";
+import ColorFood from "./ColorFood/ColorFood";
 
 const KlevleevVRElement = () => {
   const TEXT_STYLE = {
@@ -9,10 +10,27 @@ const KlevleevVRElement = () => {
     width: "100%",
   };
 
+  const ELEMENT_STYLE = {
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const COMPONENT_STYLE = {
+    marginBottom: "100px",
+  };
   return (
-    <div className="klevleev_vr_element">
-      <p style={TEXT_STYLE}>Собственный компонент</p>
-      <GuessTheSong />
+    <div className="klevleev_vr_element" style={ELEMENT_STYLE}>
+      <div style={COMPONENT_STYLE}>
+        <p style={TEXT_STYLE}>Собственный компонент</p>
+        <GuessTheSong />
+      </div>
+
+      <div style={COMPONENT_STYLE}>
+        <p style={TEXT_STYLE}>
+          Игра из <a href="https://github.com/chuckha/ColorFlood">GitHub</a>
+        </p>
+        <ColorFood width="5" height="5" />
+      </div>
     </div>
   );
 };
