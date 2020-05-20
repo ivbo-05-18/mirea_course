84e1cdf84659a8b27c245ff918acf1a260cb2ffc
leon2462 <saintmupip@gmail.com>
2020-05-20 15:17:23 +0300

    Merge branch 'master' into Ivanov_DS

--- src/components/PerederiyVA/Gauss/GaussianElimElement.js
+++ src/components/PerederiyVA/Gauss/GaussianElimElement.js
@@ -3,52 +3,61 @@ import GaussianElimResult from './GaussianElimResult';
 import './GaussianElimElement.css';
 
 class GaussianElimElement extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            matrix: [[2,3,1,17], [4,9,7,63], [-3,7,-4,-1]]
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      matrix: [[2, 3, 1, 17], [4, 9, 7, 63], [-3, 7, -4, -1]],
+    };
+  }
 
     updateMatrix = (e, i, index) => {
-        let updatedMatrix = this.state.matrix;
+      const updatedMatrix = new Array(3);
+      const { matrix } = this.state;
+      for (let j = 0; j < 3; j++) {
+        updatedMatrix[j] = matrix[j].slice();
+      }
 
-        if (parseFloat(e.target.value) || parseFloat(e.target.value) === 0) {   
-            updatedMatrix[i][index] = parseFloat(e.target.value);    
-        }
-        else { // Для удобства ввода оставить пользователю возможность вводить строки, которые не пройдут parse в float (функция GaussianElim при таких входных данных вернёт исключение)
-            updatedMatrix[i][index] = e.target.value;
-        }
+      if (parseFloat(e.target.value) || parseFloat(e.target.value) === 0) {
+        updatedMatrix[i][index] = parseFloat(e.target.value);
+      } else {
+        // Для удобства ввода оставить пользователю возможность вводить строки, которые не
+        // пройдут parse в float (функция GaussianElim при таких входных данных вернёт исключение)
+        updatedMatrix[i][index] = e.target.value;
+      }
 
-        this.setState({
-            matrix: updatedMatrix 
-        })
+      this.setState({
+        matrix: updatedMatrix,
+      });
     }
 
     render() {
-        return (
-            <div class="perederiyva_gauss_input">
-                <table>
-                    {this.state.matrix.map((item, i) => (
-                        <tr>
-                            <td>
-                                <input type="number" step="any" value={item[0]} onChange={(e) => this.updateMatrix(e, i, 0)} />x +
-                            </td>
-                            <td>
-                                <input type="number" step="any" value={item[1]} onChange={(e) => this.updateMatrix(e, i, 1)} />y +
-                            </td>
-                            <td>
-                                <input type="number" step="any" value={item[2]} onChange={(e) => this.updateMatrix(e, i, 2)} />z =
-                            </td>
-                            <td>
-                                <input type="number" step="any" value={item[3]} onChange={(e) => this.updateMatrix(e, i, 3)} />
-                            </td>
-                        </tr>
-                    ))}
-                </table>   
-                <GaussianElimResult input={this.state.matrix} />
-            </div>
-        )
+      const { matrix } = this.state;
+      return (
+        <div className="perederiyva_gauss_input">
+          <table>
+            {matrix.map((item, i) => (
+              <tr>
+                <td>
+                  <input type="number" step="any" value={item[0]} onChange={(e) => this.updateMatrix(e, i, 0)} />
+                  x +
+                </td>
+                <td>
+                  <input type="number" step="any" value={item[1]} onChange={(e) => this.updateMatrix(e, i, 1)} />
+                  y +
+                </td>
+                <td>
+                  <input type="number" step="any" value={item[2]} onChange={(e) => this.updateMatrix(e, i, 2)} />
+                  z =
+                </td>
+                <td>
+                  <input type="number" step="any" value={item[3]} onChange={(e) => this.updateMatrix(e, i, 3)} />
+                </td>
+              </tr>
+            ))}
+          </table>
+          <GaussianElimResult input={matrix} />
+        </div>
+      );
     }
 }
 
