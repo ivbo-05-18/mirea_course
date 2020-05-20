a0c5bf05f9644d06e25a895f94cfb63f338e5438
ballbuster2005 <61076179+ballbuster2005@users.noreply.github.com>
2020-05-20 16:17:00 +0300

    Eslint

--- src/components/ZolototrubovER/ZolototrubovERController.js
+++ src/components/ZolototrubovER/ZolototrubovERController.js
@@ -1,23 +1,23 @@
-import React, {useState} from 'react'
-import ZolototrubovERElement from './ZolototrubovERElement'
+import React, { useState } from 'react';
+import ZolototrubovERElement from './ZolototrubovERElement';
 
 const ZolototrubovERController = () => {
-    const [showState, switchState ] = useState({
-        show: false
-    })
+  const [showState, switchState] = useState({
+    show: false,
+  });
 
-    const switchShowState = () => {
-        switchState({
-            show: !showState.show
-        })
-    }
+  const switchShowState = () => {
+    switchState({
+      show: !showState.show,
+    });
+  };
 
-    return (
-        <div className="zolototrubov_er">
-            <button onClick={switchShowState}> Золототрубов Е.Р. </button>
-            {showState.show && <ZolototrubovERElement/>}
-        </div>
-    )
-}
+  return (
+    <div className="zolototrubov_er">
+      <button onClick={switchShowState} type="submit"> Золототрубов Е.Р. </button>
+      {showState.show && <ZolototrubovERElement />}
+    </div>
+  );
+};
 
-export default ZolototrubovERController
+export default ZolototrubovERController;
--- src/components/ZolototrubovER/ZolototrubovERElement.js
+++ src/components/ZolototrubovER/ZolototrubovERElement.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import CharInfo from './CharInfo'
+import React from 'react';
+import CharInfo from './CharInfo';
 
 
 const ZolototrubovERElement = () => {
-    const TEXT_STYLE = {
-        'font-size' : '25px',
-        color       : 'white',
-        margin      : '10px 0px'
-    }
+  const TEXT_STYLE = {
+    'font-size': '25px',
+    color: 'white',
+    margin: '10px 0px',
+  };
 
-    return (
-        <div className="zolototrubov_element">
-            <p style={TEXT_STYLE}>Собственный компонент</p>
-            <CharInfo/>
-        </div>
-    )
-}
+  return (
+    <div className="zolototrubov_element">
+      <p style={TEXT_STYLE}>Собственный компонент</p>
+      <CharInfo />
+    </div>
+  );
+};
 
-export default ZolototrubovERElement
+export default ZolototrubovERElement;
