a20ae8b6474982e499801c7fa55d89677f971663
Anime-Girl <yoroshikuqq@mail.ru>
2020-05-22 21:46:17 +0300

    fixed cmyk test

--- src/components/MiroshnikGK/tests/CMYK.test.js
+++ src/components/MiroshnikGK/tests/CMYK.test.js
@@ -1,39 +1,43 @@
 import hexToCMYK from '../Color/hextocmyk';
 
-describe('hex code to cmyk expression', () => {
-  test('hex to cmyk', () => {
+describe('hex to cmyk with right string', () => {
+  test('hex to cmyk success', () => {
     expect(hexToCMYK('#000000')).toBe('0 0 0 1');
   });
 });
 
-describe('hex code to cmyk expression', () => {
-  test('hex to cmyk', () => {
+describe('hex to cmyk with inappropriate string', () => {
+  test('with wrong quantity of symbols', () => {
     expect(hexToCMYK('#123')).toBe('');
   });
-});
-
-describe('hex code to cmyk expression', () => {
-  test('hex to cmyk', () => {
+  test('with no #', () => {
     expect(hexToCMYK('123123')).toBe('');
   });
-});
-
-describe('hex code to cmyk expression', () => {
-  test('hex to cmyk', () => {
+  test('with wrong string format', () => {
     expect(hexToCMYK('blue')).toBe('');
   });
 });
 
-describe('hex code to cmyk expression', () => {
-  test('hex to cmyk', () => {
+
+describe('hex to cmyk with wrong input data', () => {
+  test('with list', () => {
     const a = { 1: 'a', 2: 'b', 3: 'c' };
     expect(hexToCMYK(a)).toBeUndefined();
   });
-});
-
-describe('hex code to cmyk expression', () => {
-  test('hex to cmyk', () => {
+  test('with number', () => {
     const a = 123;
+    expect(hexToCMYK(a)).toBeUndefined();
+  });
+  test('with null', () => {
+    const a = null;
+    expect(hexToCMYK(a)).toBeUndefined();
+  });
+  test('with boolean', () => {
+    const a = true;
+    expect(hexToCMYK(a)).toBeUndefined();
+  });
+  test('with function', () => {
+    const a = hexToCMYK;
     expect(hexToCMYK(a)).toBeUndefined();
   });
 });
