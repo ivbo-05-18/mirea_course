113a1943795ecce8016c5d5cb1ed6efce8c992cd
Vyacheslav <slava.klevleev@gmail.com>
2020-05-29 02:00:00 +0300

    Component of another student
    To-Do List

--- src/components/VelikanovKY/VelikanovKYElement.js
+++ src/components/VelikanovKY/VelikanovKYElement.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Weather from './Weather';
 import Trends from './Trends';
+import TodoApp from './KlevleevVRToDo';
 
+const todoItems = [];
+todoItems.push({ index: 1, value: 'Сходить в магазин', done: false });
+todoItems.push({ index: 2, value: 'Полить цветы', done: true });
+todoItems.push({ index: 3, value: 'Покормить кота', done: true });
 
 const VelikanovKYElement = () => (
   <div>
@@ -10,6 +15,9 @@ const VelikanovKYElement = () => (
     <hr />
     <h2>Элемент извне</h2>
     <Trends />
+    <hr />
+    <h2>Элемент другого студента (Клевлеев В.Р.)</h2>
+    <TodoApp initItems={todoItems} />
     <hr />
   </div>
 );
