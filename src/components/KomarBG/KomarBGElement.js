ed971531801e15217eaa84ae01d78136a8ed9a74
Komar Bogdan <chompski@inbox.ru>
2020-05-28 21:09:00 +0300

    Added 2 more components

--- src/components/KomarBG/KomarBGElement.js
+++ src/components/KomarBG/KomarBGElement.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import MemeCreator from './MemeCreator/MemeCreator';
+import Minesweeper from './MineSweeper/Minesweeper';
+import KorneevDSElement from './KorneevDS/KorneevDSElement';
 
 export default function KomarBGElement() {
   const TEXT_STYLE = {
     fontSize: '25px',
     color: 'black',
     margin: '10px 0px',
+    fontWeight: 'bold',
   };
 
   const ELEMENT_STYLE = {
@@ -19,6 +22,12 @@ export default function KomarBGElement() {
     <div className="komar-bg_element" style={ELEMENT_STYLE}>
       <p style={TEXT_STYLE}>Собственный компонент</p>
       <MemeCreator />
+      <hr />
+      <p style={TEXT_STYLE}>Заимственный компонент с GitHub</p>
+      <Minesweeper />
+      <hr />
+      <p style={TEXT_STYLE}>Заимственный компонент (Корнеев Д.С.) </p>
+      <KorneevDSElement />
     </div>
   );
 }
