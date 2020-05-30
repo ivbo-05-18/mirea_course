bc6e082f0cd5638c163eae5d65af1ca0129c8d79
Meliser <mitya.korneev@mail.ru>
2020-05-30 20:33:04 +0300

    Fix

--- src/components/KorneevDS/KorneevDSElement.js
+++ src/components/KorneevDS/KorneevDSElement.js
@@ -4,22 +4,22 @@ import Leaderboard from '../PetrovSD/Leaderboard';
 import TicTacToe from './react-tic-tac-toe';
 
 const STYLE = {
-  'border-radius': '2px',
+  borderRadius: '2px',
   background: '#999',
   margin: '10px',
   padding: '5px 25px',
 };
 
 const BLOCK_STYLE = {
-  'max-width': 900,
+  maxWidth: 900,
   display: 'flex',
-  'flex-direction': 'row',
-  'justify-content': 'center',
-  'font-size': '16px',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  fontSize: '16px',
 };
 
 const TEXT_STYLE = {
-  'font-size': '16px',
+  fontSize: '16px',
   color: 'black',
   margin: '10px 0px',
 };
