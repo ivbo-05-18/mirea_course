edbf54d546a589e0a59e22cd60e0664671515111
Alex <alex9999091@gmail.com>
2020-06-03 21:50:10 +0300

    Fixed ESlint rules

--- src/components/TarasovAA/Greetings/Flag.js
+++ src/components/TarasovAA/Greetings/Flag.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Flag extends React.Component {
-  render() {
-    const { Greet } = this.props;
-    return (
-      <img alt="flag" data-testid="flag-img" src={`https://www.countryflags.io/${Greet.alphacode}/shiny/64.png`} />
-    );
-  }
+export default function Flag(props) {
+  const { Greet } = props;
+  return (
+    <img alt="flag" data-testid="flag-img" src={`https://www.countryflags.io/${Greet.alphacode}/shiny/64.png`} />
+  );
 }
 
 Flag.propTypes = {
